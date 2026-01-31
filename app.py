b961e65bcc302642fd110307cfa4674910e1ff73
Harvin Ong <harvin.ong20@gmail.com>
2026-01-31 16:21:19 +0800

    feat: add user routes

--- app.py
+++ app.py
@@ -6,9 +6,17 @@ app: Flask = Flask(__name__)
 def index():
     return render_template("main.html")
 
+@app.get("/user")
+def selfUser():
+    return "Hello, user! :3"
+
+@app.get("/@<user>")
+def user(user:str):
+    return f"Hello, {user}!"
+
 @app.get("/<nonexistent>")
 def notFound(nonexistent):
     return f"<h1>404 Not Found</h1><p>\"{nonexistent}\" does not exist.</p><p>Please consider going outside and touching grass!</p>"
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug = True)
