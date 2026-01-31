3c5e777a16bd6f1a51bfbbb5526125c32d98b459
Harvin Ong <harvin.ong20@gmail.com>
2026-01-31 19:32:33 +0800

    feat: fully crud User class

--- crud.py
+++ crud.py
@@ -2,6 +2,8 @@
 import pymongo, os
 from datetime import datetime
 from pprint import pprint
+from bson.objectid import ObjectId
+from typing import Any
 
 MONGODB_URI: str = os.environ["MONGODB_URI"]
 CLIENT = pymongo.MongoClient(MONGODB_URI)
@@ -11,7 +13,7 @@ MYDB = CLIENT["JalaArtMarket"]
 class User:
     _DBCOLLECTION = MYDB["users"]
 
-    def __init__(self, username: str, passHash: str, fullName: str, surName: str|None = None, creationDate: datetime = datetime.now(), country: str|None = None, website: str|None = None) -> None:
+    def __init__(self, username: str, passHash: str, fullName: str, surName: str|None = None, creationDate: datetime = datetime.now(), country: str|None = None, website: str|None = None, _id: ObjectId|None = None) -> None:
         self.username: str = username
         self.passHash: str = passHash
         self.fullName: str = fullName
@@ -20,13 +22,33 @@ class User:
         self.country: str|None = country
         self.website: str|None = website
 
+        existingData = self.findItem(self.username)
+        if existingData is None:
+            self._id = self._insertItem()
+        else:
+            self._id = existingData.get("_id")
+
     @staticmethod
-    def getItem(username: str):
-        return User._DBCOLLECTION.find_one({"username": username})
+    def fromDict(data: dict) -> User:
+        return User(**data)
+
+    @staticmethod
+    def findItem(username: str) -> dict[str, Any]|None:
+        userdata = User._DBCOLLECTION.find_one({"username": username})
+        return userdata
     
-    def setItem(self) -> int:
-        return self._DBCOLLECTION.insert_one(self.__dict__).inserted_id
+    def _insertItem(self) -> ObjectId|None:
+        existingData = self.findItem(self.username)
+        if existingData is None:
+            idNum = self._DBCOLLECTION.insert_one(self.__dict__).inserted_id
+            return idNum
     
+    def update(self) -> None:
+        self._DBCOLLECTION.find_one_and_update({"_id": self._id}, {"$set": self.__dict__})
+    
+    def deleteItem(self) -> None:
+        self._DBCOLLECTION.delete_one({"_id": self._id})
+
 
 if __name__ == "__main__":
     print(MYDB.list_collection_names())
@@ -46,7 +68,3 @@ if __name__ == "__main__":
     #     User("@jackchris", "xxxxxx", "Jack", country = "Indonesia"),
     #     User("@whansel", "xxxxxx", "William", country = "Indonesia")
     # ]
-
-    # for inst in userinstances:
-    #     pprint(User.getItem(inst.username))
-    pprint(User.getItem("@harvinong"))
